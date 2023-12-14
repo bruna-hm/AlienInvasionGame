057ad11387555247a2d698f642b01ff9df1b8bde
Bruna <gitbruna@fake.com>
2023-12-14 20:50:43 -0300

    dia 14dez23 2050

---
+++ alien_inavsion.py
@@ -0,0 +1,12 @@
+import sys
+import pygame
+
+class AlienInvasion:
+    """Classe overall para gerenciar assets e comportamento"""
+
+    def __init__(self,):
+        ""Inicializar o jogo e criar recursos de jogo""""
+        
+    pygame.init()
+    
+    self.screen = pygame.display
