3a639925814d24bfb9f304b7ec000241e2f3e8a6
Bruna <gitbruna@fake.com>
2024-03-21 17:36:47 -0300

    21/03/24

--- alien.py
+++ alien.py
@@ -7,6 +7,7 @@ class Alien(Sprite):
     def __init__(self, ai_game):
         super().__init__()
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         
         self.image = pygame.image.load('AlienInvasionGame\images\Alien.BMP')
         self.rect = self.image.get_rect()
@@ -15,5 +16,13 @@ class Alien(Sprite):
         self.rect.y = self.rect.height
         
         self.x = float(self.rect.x)
+        
+    def check_edges(self):
+        screen_rect = self.screen.get.rect()
+        return (self.rect.right >= screen_rect.right) or (self.rect.left <= 0)
+    
+    def update(self):
+        self.x += self.settings.alien_speed * self.settings.fleet_direction
+        self.rect.x = self.x
         
         
