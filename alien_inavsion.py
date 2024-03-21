5425d4fd5a599f98b4501d9b6dbe702874f2840d
Bruna <gitbruna@fake.com>
2024-03-21 17:42:48 -0300

    21/03/24

--- alien_inavsion.py
+++ alien_inavsion.py
@@ -1,6 +1,8 @@
 import sys
+from time import sleep
 import pygame
 from settings import Settings
+from game_stats import GameStats
 from ship import Ship
 from alien import Alien
 from bullet import Bullet
@@ -18,12 +20,16 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Invasão Alienígena!!!")
+        
+        self.stats = GameStats(self)
     
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
         self.aliens = pygame.sprite.Group()
         
-        #self._create_fleet()
+        self._create_fleet()
+        
+        self.game_active = True
     
     def run_game(self):
         """Inicia o loop principal, main para o jogo"""
@@ -31,6 +37,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
             self.clock.tick(60)
     
@@ -84,7 +91,27 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
                 if bullet.rect.bottom <= 0:
                     self.bullets.remove(bullet)
+                    
+        self._check_bullet_alien_collisions()
+        
+    def _check_bullet_alien_collisions(self):            
+        collisions = pygame.sprite.groupcollide(
+            self.bullets, self.aliens, True, True)
+        
+        if not self.aliens:
+            self.bullets.empty()
+            self._create_fleet()
+                    
+    def _update_aliens(self):
+        self.check_fleet_edges()
+        self.aliens.update()
+        
+        if pygame.sprite.spritecollideany(self.ship, self.aliens):
+            self._ship_hit()
+            print("Shit HIT!!!")
             
+            self._check_aliens_bottom()
+        
     def _update_screen(self):
         #Atualiza imagens da tela, e flip para nova tela
         self.screen.fill(self.settings.bg_color)
@@ -94,7 +121,59 @@ class AlienInvasion:
         self.aliens.draw(self.screen)
         
         pygame.display.flip()
+        
+    def _create_fleet(self):
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        current_x, current_y = alien_width, alien_height
+        while current_y < (self.settings.screen.height - 3 * alien_height):
+            while current_x < (self.settings.screen_width - 2 * alien_width):
+                self._create_alien(current_x, current_y)
+                current_x += 2 * alien_width
+                
+            current_x = alien_width
+            current_y += 2 * alien_height
             
+    def _create_alien(self, x_position, y_position):
+        new_alien = Alien(self)
+        new_alien.x = x_position
+        new_alien.rect.x = x_position
+        new_alien.rect.y = y_position
+        self.aliens.add(new_alien)
+        
+    def _check_fleet_edges(self):
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+            
+    def _change_fleet_direction(self):
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+        
+    def _ship_hit(self):
+        if self.stats.ships_left > 0:
+            self.stats.ships_left -= 1
+        
+            self.bullets.empty()
+            self.aliens.empty()
+        
+            self._create_fleet()
+            self.ship.center_ship()
+        
+            sleep(0.5)
+        
+        else:
+            self.game_active = False
+            
+            
+    def _check_aliens_botttom(self):
+        for alien in self.aliens.sprites():
+            if alien.rect.bottom >= self.settings.screen_height:
+                self._ship_hit()
+                break
+        
 if __name__ == '__main__':
     #Faça uma instância do jogo e run.
     ai = AlienInvasion()
